f922195fb045f14e983b41bee1bf765e275ea1bb
Tommy Snelson <tommysnelson49@gmail.com>
2022-10-14 18:31:47 +0100

    Scraper for LAQ

---
+++ third-year-project/album_scrape.py
@@ -0,0 +1,89 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+
+
+class AlbumScrape:
+
+    def __init__(self, url, album_tag, album_class, artist_tag, artist_class, page_arr=None, extra_urls=None):
+
+        service = Service(executable_path=ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service)
+        self.url = url
+        self.urls = []
+        self.page_arr = page_arr
+        self.album_tag = album_tag
+        self.album_class = album_class
+        self.artist_tag = artist_tag
+        self.artist_class = artist_class
+        self.records = []
+        self.extra_urls = extra_urls
+        self.artists = []
+        self.albums = []
+
+    def get_page_urls(self):
+
+        if self.page_arr is None:
+
+            self.page_arr = [x for x in range(1, 10001)]
+
+        for x in self.page_arr:
+
+            self.urls += [self.url.format(x)]
+
+        self.urls += self.extra_urls
+
+    def get_data(self):
+
+        artists = []
+        albums = []
+
+        self.get_page_urls()
+
+        for url in self.urls:
+
+            try:
+                self.driver.get(url)
+            except:
+                break
+
+            content = self.driver.page_source
+
+            soup = BeautifulSoup(content, features="html.parser")
+
+            albums_soup, artists_soup = self.get_artist_album(soup)
+
+            artists += artists_soup
+            albums += albums_soup
+
+        self.records = [[album, artist] for album, artist in zip(albums, artists)]
+
+    def get_artist_album(self, soup):
+
+        artists_soup = soup.find_all(self.artist_tag, self.artist_class)
+        albums_soup = soup.find_all(self.album_tag, self.album_class)
+
+        return [album.text for album in albums_soup], [artist.text for artist in artists_soup]
+
+    def get_records(self):
+
+        return self.records
+
+
+class LAQAlbumScrape(AlbumScrape):
+
+    def __init__(self, url, album_tag, album_class, artist_tag, artist_class, page_arr=None, extra_urls=None):
+
+        super().__init__(url, album_tag, album_class, artist_tag, artist_class, page_arr, extra_urls)
+
+    def get_artist_album(self, soup):
+
+        artists_albums_soup = soup.find_all(self.artist_tag, self.artist_class)
+        artists_albums_soup = [artist_album.a.get_text(strip=True, separator='\n').splitlines() for artist_album in
+                               artists_albums_soup]
+
+        artist_soup = [artists_albums_soup[i][0] for i in range(0, len(artists_albums_soup))]
+        album_soup = [artists_albums_soup[i][1] for i in range(0, len(artists_albums_soup))]
+
+        return album_soup, artist_soup
--- third-year-project/all_music.py
+++ third-year-project/all_music.py
@@ -2,6 +2,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
+from album_scrape import AlbumScrape
+from album_scrape import LAQAlbumScrape
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
@@ -10,24 +12,20 @@ i = 0
 artists = []
 albums = []
 
-while True:
+# pitchfork_scrape = AlbumScrape("https://pitchfork.com/reviews/albums/?page={}", "h2", {"class": "review__title-album"},
+#                                "ul", {"class": "artist-list review__title-artist"}, [x for x in range(1, 6)])
+#
+# pitchfork_scrape.get_data()
+# print(pitchfork_scrape.get_records())
 
-    i += 1
+# Need to work out how to split on <br>
 
-    try:
-        driver.get("https://pitchfork.com/reviews/albums/?page={}".format(i))
-    except:
-        break
+# laq_scrape = LAQAlbumScrape(url="https://www.loudandquiet.com/reviews/page/{}/", album_tag="div",
+#                             album_class={"class": "loudandquiet-listblock"}, artist_tag="div",
+#                             artist_class={"class": "loudandquiet-listblock"}, page_arr=[2],
+#                             extra_urls=["https://www.loudandquiet.com/reviews/"])
+#
+# laq_scrape.get_data()
+# laq_scrape.get_records()
 
-    content = driver.page_source
 
-    soup = BeautifulSoup(content, features="html.parser")
-
-    artists_soup = soup.find_all("ul", {"class": "artist-list review__title-artist"})
-    albums_soup = soup.find_all("h2", {"class": "review__title-album"})
-
-    artists += [artist.text for artist in artists_soup]
-    albums += [album.text for album in albums_soup]
-
-records = [[album, artist] for album, artist in zip(albums, artists)]
-print(records)
