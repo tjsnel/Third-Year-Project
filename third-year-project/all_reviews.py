6d724f51bf5f340bd779318cbcc3c9fa02c16582
Tommy Snelson <tommysnelson49@gmail.com>
2022-11-01 22:54:11 +0000

    Initial EDA and pyplot analysis of scores and genres.

--- third-year-project/all_reviews.py
+++ third-year-project/all_reviews.py
@@ -1,42 +1,42 @@
 from review_scrape import ReviewScrape, GuardianReviewScrape, GenreReviewScrape, PitchforkReviewScrape, NMEReviewScrape
 import pandas as pd
 
-# pitchfork_df = pd.read_csv("C:\\Users\\tommy\\OneDrive\\Third Year Project\\Platform Album Data\\urls_shared.csv",
-#                            index_col=0)
-# pitchfork_scrape = PitchforkReviewScrape(df=pitchfork_df,
-#                                          platform="Pitchfork",
-#                                          header_tag="div",
-#                                          header_class={"class": "BaseWrap-sc-UrHlS BaseText-fFrHpW "
-#                                                                 "SplitScreenContentHeaderDekDown-fkATUS "
-#                                                                 "boMZdO kqRnRO fZZtlk"},
-#                                          body_tag="div",
-#                                          body_class="body__inner-container",
-#                                          score_tag="div",
-#                                          score_class1={"class": "ScoreCircle-cIILhI hUIQbu"},
-#                                          score_class2={"class": "ScoreCircle-cIILhI hHVylS"},
-#                                          genre_tag="p",
-#                                          genre_class={
-#                                              "class": "BaseWrap-sc-UrHlS BaseText-fFrHpW InfoSliceValue-gTHtZf "
-#                                                       "boMZdO cdnzYc jNiPXA"})
+pitchfork_df = pd.read_csv("C:\\Users\\tommy\\OneDrive\\Third Year Project\\Platform Album Data\\urls_shared.csv",
+                           index_col=0)
+pitchfork_scrape = PitchforkReviewScrape(df=pitchfork_df,
+                                         platform="Pitchfork",
+                                         header_tag="div",
+                                         header_class={"class": "BaseWrap-sc-UrHlS BaseText-fFrHpW "
+                                                                "SplitScreenContentHeaderDekDown-fkATUS "
+                                                                "boMZdO kqRnRO fZZtlk"},
+                                         body_tag="div",
+                                         body_class="body__inner-container",
+                                         score_tag="div",
+                                         score_class1={"class": "ScoreCircle-cIILhI hUIQbu"},
+                                         score_class2={"class": "ScoreCircle-cIILhI hHVylS"},
+                                         genre_tag="p",
+                                         genre_class={
+                                             "class": "BaseWrap-sc-UrHlS BaseText-fFrHpW InfoSliceValue-gTHtZf "
+                                                      "boMZdO cdnzYc jNiPXA"})
+
+pitchfork_scrape.get_data()
+pitchfork_scrape.write_records()
+
+# guardian_df = pd.read_csv("C:\\Users\\tommy\\OneDrive\\Third Year Project\\Platform Album Data\\urls_shared.csv",
+#                           index_col=0)
+# guardian_scrape = GuardianReviewScrape(df=guardian_df,
+#                                        platform="Guardian",
+#                                        header_tag="div",
+#                                        header_class={"class": "dcr-1aw10i3"},
+#                                        body_tag=["span", "p"],
+#                                        body_class={"class": "dcr-hw2voq"},
+#                                        score_tag="path",
+#                                        score_class={"fill": "transparent"},
+#                                        genre_tag="li",
+#                                        genre_class={"class": "dcr-1jcr6mo"})
 #
-# pitchfork_scrape.get_data()
-# pitchfork_scrape.write_records()
-
-guardian_df = pd.read_csv("C:\\Users\\tommy\\OneDrive\\Third Year Project\\Platform Album Data\\urls_shared.csv",
-                          index_col=0)
-guardian_scrape = GuardianReviewScrape(df=guardian_df,
-                                       platform="Guardian",
-                                       header_tag="div",
-                                       header_class={"class": "dcr-1aw10i3"},
-                                       body_tag=["span", "p"],
-                                       body_class={"class": "dcr-hw2voq"},
-                                       score_tag="path",
-                                       score_class={"fill": "transparent"},
-                                       genre_tag="li",
-                                       genre_class={"class": "dcr-1jcr6mo"})
-
-guardian_scrape.get_data()
-guardian_scrape.write_records()
+# guardian_scrape.get_data()
+# guardian_scrape.write_records()
 
 # spectrum_df = pd.read_csv("C:\\Users\\tommy\\OneDrive\\Third Year Project\\Platform Album Data\\urls_shared.csv",
 #                           index_col=0)
